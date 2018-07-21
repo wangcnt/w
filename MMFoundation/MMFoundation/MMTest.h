fa2851c2b96d1dd1dc0fa6b7a1aa171654c2a5fd
wangcnt <404298011@qq.com>
2018-07-22 00:03:38 +0800

    MMFoundation.framework

---
+++ MMFoundation/MMFoundation/MMTest.h
@@ -0,0 +1,15 @@
+//
+//  MMTest.h
+//  MMFoundation
+//
+//  Created by Mark on 2018/7/21.
+//  Copyright © 2018年 Mark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MMTest : NSObject
+
+- (void)hello;
+
+@end
---
+++ MMFoundation/MMFoundation/MMTest.m
@@ -0,0 +1,17 @@
+//
+//  MMTest.m
+//  MMFoundation
+//
+//  Created by Mark on 2018/7/21.
+//  Copyright © 2018年 Mark. All rights reserved.
+//
+
+#import "MMTest.h"
+
+@implementation MMTest
+
+- (void)hello {
+    NSLog(@"HEllo");
+}
+
+@end
